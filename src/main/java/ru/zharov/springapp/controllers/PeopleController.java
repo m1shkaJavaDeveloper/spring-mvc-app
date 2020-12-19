c564f57786b7d886ea9177c9ad64bc85242e364f
Михаил <m1shka1998@mail.ru>
2020-12-19 16:36:46 +0300

    CRUD_REST_DAO

---
+++ src/main/java/ru/zharov/springapp/controllers/PeopleController.java
@@ -0,0 +1,37 @@
+package ru.zharov.springapp.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import ru.zharov.springapp.dao.PersonDAO;
+
+@Controller
+@RequestMapping("/people")
+public class PeopleController {
+
+    private final PersonDAO personDAO;
+
+    @Autowired
+    public PeopleController(PersonDAO personDAO) {
+        this.personDAO = personDAO;
+    }
+
+    @GetMapping()
+    public String index(Model model){
+        //Получим все людей из DAO и передадим на отображение в представление
+        model.addAttribute("people", personDAO.index());
+        return "people/index";
+    }
+
+    @GetMapping("/{id}")
+    public String show(@PathVariable("id") int id,
+                       Model model){
+        //Получим 1 человека и передадим на отображение в представление
+        model.addAttribute("person", personDAO.show(id));
+        return "people/show";
+    }
+}
