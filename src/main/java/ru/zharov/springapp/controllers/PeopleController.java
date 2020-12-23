607cf99878b67f985f48ad004396f2b47b888b6b
Михаил <m1shka1998@mail.ru>
2020-12-23 14:19:00 +0300

    Validation

--- src/main/java/ru/zharov/springapp/controllers/PeopleController.java
+++ src/main/java/ru/zharov/springapp/controllers/PeopleController.java
@@ -3,9 +3,12 @@ package ru.zharov.springapp.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import ru.zharov.springapp.dao.PersonDAO;
 import ru.zharov.springapp.models.Person;
+
+import javax.validation.Valid;
 
 @Controller
 @RequestMapping("/people")
@@ -39,7 +42,10 @@ public class PeopleController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("person") Person person){
+    public String create(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+            return "people/new";
         personDAO.save(person);
         return "redirect:/people";
     }
@@ -51,8 +57,12 @@ public class PeopleController {
     }
 
     @PatchMapping("/{id}")
-    public String update(@ModelAttribute("person") Person person,
+    public String update(@ModelAttribute("person") @Valid Person person,
+                         BindingResult bindingResult,
                          @PathVariable("id") int id){
+        if(bindingResult.hasErrors())
+            return "people/edit";
+
         personDAO.update(id, person);
         return "redirect:/people";
     }
