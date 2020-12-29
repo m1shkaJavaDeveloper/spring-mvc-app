84c5223617811a71b5e1a58e49a48ac11007d90e
Михаил <m1shka1998@mail.ru>
2020-12-29 13:08:45 +0300

    JDBCTemplate

--- src/main/java/ru/zharov/springapp/config/SpringConfig.java
+++ src/main/java/ru/zharov/springapp/config/SpringConfig.java
@@ -5,12 +5,16 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
 import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan("ru.zharov.springapp")
@@ -46,5 +50,22 @@ public class SpringConfig implements WebMvcConfigurer {
         ThymeleafViewResolver resolver = new ThymeleafViewResolver();
         resolver.setTemplateEngine(templateEngine());
         registry.viewResolver(resolver);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dateSource = new DriverManagerDataSource();
+
+        dateSource.setDriverClassName("org.postgresql.Driver");
+        dateSource.setUrl("jdbc:postgresql://localhost:5433/first_db");
+        dateSource.setUsername("postgres");
+        dateSource.setPassword("m1shka1998cs16");
+
+        return dateSource;
+    }
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(){
+        return new JdbcTemplate(dataSource());
     }
 }
